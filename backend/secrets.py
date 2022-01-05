197c6ee72a2d5978fccc23b8085d83e4f3ca126a
ojayjonathan <jonathanonderi2018@yahoo.com>
2022-01-05 16:10:48 +0300

    update

---
+++ backend/secrets.py
@@ -0,0 +1,9 @@
+class config:
+    SECRET_KEY ="odfjidfoweifjs;j9e"
+    development =True
+    ALLOWED_HOSTS = ["*"]
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = "config.EMAIL_HOST"
+    EMAIL_HOST_USER = "config.EMAIL_HOST_USER"
+    EMAIL_HOST_PASSWORD = "config.EMAIL_HOST_PASSWORD"
+    PORT = 8186
