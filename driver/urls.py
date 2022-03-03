646cf56128f3661fa823bd1c3e82da99d9914b55
ojayjonathan <jonathanonderi2018@yahoo.com>
2022-03-03 12:50:52 +0300

    append / on urls

--- driver/urls.py
+++ driver/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from driver.views import DriverProfileView,VehicleView
 
 urlpatterns = [
-    path('driver/profile', DriverProfileView.as_view()),
-    path("driver/vehicle", VehicleView.as_view(), name="vehicle"),
+    path('driver/profile/', DriverProfileView.as_view()),
+    path("driver/vehicle/", VehicleView.as_view(), name="vehicle"),
 ]
